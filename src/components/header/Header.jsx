9ec7d72820bcf482b627ee4f9735dac032160f90
rajendra dhaka <rajendradhaka13@gmail.com>
2022-12-21 19:48:34 +0530

    added typewriter styles to header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,6 +3,7 @@ import CTA from './CTA';
 import ME from '../../assets/me.png';
 import HeaderSocials from './HeaderSocials';
 import './header.css';
+import { Typewriter } from 'react-simple-typewriter';
 
 export const Header = () => {
   return (
@@ -10,8 +11,17 @@ export const Header = () => {
       <div className='container header__container'>
         <h5>Hello I'm</h5>
         <h1>Rajendra Dhaka</h1>
-        <h5 className='text-light'>Frontend Developer</h5>
-        <h5 className='text-light'>Aspiring Fullstack Developer</h5>
+        <h5 className='text-light'>
+          <Typewriter
+            words={['Frontend Developer', 'Aspiring to be Fullstack', 'Thank You for Visit !']}
+            loop
+            cursor
+            cursorStyle='😀'
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={1000}
+          />
+        </h5>
         <CTA />
         <HeaderSocials />
         <div className='me'>
